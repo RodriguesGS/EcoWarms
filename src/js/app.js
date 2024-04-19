f3a4b6b6d07582a0e32d76f6f063a514c0d1a2e0
unknown <rodriguesgasoares@gmail.com>
2024-04-19 17:50:12 -0300

    Feat: Adicinando a API de pesquisa climática

--- src/js/app.js
+++ src/js/app.js
@@ -24,10 +24,73 @@ document.addEventListener("click", (e) => {
 
 // Modal
 
-document.querySelector("#openModal").addEventListener("click", () => {
-    document.querySelector("#modal").classList.remove("hidden")
-})
-
 document.querySelector("#closeModal").addEventListener("click", () => {
     document.querySelector("#modal").classList.add("hidden")
+    document.body.classList.toggle("modal-open")
+})
+
+// API weather
+
+const API_KEY = "f68bb9bcd75825343655d1dc14d621ab"
+const APICountryURL = "https://countryflagsapi.com/png/";
+
+const inputSearh = document.querySelector("#search")
+const citySearch = document.querySelector("#searchAPI")
+
+const city = document.querySelector("#city")
+const temp = document.querySelector("#temperature span")
+const weatherImg = document.querySelector("#weatherImg")
+const countryImg = document.querySelector("#country");
+const humidity = document.querySelector("#humidity span");
+const wind = document.querySelector("#wind span");
+
+const getWeatherData = async (cityName) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY }&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+}
+
+const showWeather = async (cityName) => {
+    const data = await getWeatherData(cityName)
+
+    city.innerText = data.name
+    temp.innerText = parseInt(data.main.temp)
+    weatherImg.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+
+    countryImg.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+
+    humidity.innerText = `${data.main.humidity}%`
+    wind.innerText = `${data.wind.speed} km/h`
+}
+
+citySearch.addEventListener("click", async (e) => {
+    const cityName = e.target.value
+
+    document.querySelector("#modal").classList.remove("hidden")
+
+    document.body.classList.toggle("modal-open")
+    
+    inputSearh.value = ""
+
+    showWeather(cityName)
+})
+
+inputSearh.addEventListener("keyup", e => {
+    if (e.code === "Enter") {
+        const cityName = e.target.value
+
+        document.querySelector("#modal").classList.remove("hidden")
+
+        inputSearh.value = ""
+
+        document.body.classList.toggle("modal-open")
+
+        showWeather(cityName)
+    }
 })
